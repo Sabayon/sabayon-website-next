580d4843bff3e96bc353f9f083180a4a1ac2b9dc
mudler <mudler@sabayon.org>
2016-02-20 00:01:17 +0100

    adding transparent navbar and shadow effect

--- static/js/script.js
+++ static/js/script.js
@@ -15,3 +15,26 @@ $(this).attr('target', '_blank');
 );
 
 });
+
+
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('#flavours');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top-150) {
+          $(".navbar-default").css('background', '#000',"!important");
+          $(".navbar-default").css('-webkit-box-shadow','0 8px 6px -6px #999');
+          $(".navbar-default").css('-moz-box-shadow','0 8px 6px -6px #999');
+          $(".navbar-default").css('box-shadow','0 8px 6px -6px #999');
+       } else {
+          $('.navbar-default').css('background', 'transparent');
+          $(".navbar-default").css('-webkit-box-shadow','none');
+          $(".navbar-default").css('-moz-box-shadow','none');
+          $(".navbar-default").css('box-shadow','none');
+       }
+   });
+    }
+});
