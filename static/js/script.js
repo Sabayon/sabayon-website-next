263ec931812efe8ec8ace9c2d0620fee1e129910
André Jaenisch <Ryuno-Ki@users.noreply.github.com>
2016-10-29 22:01:23 +0200

    Shorten forum feed. Fixes #41 (#42)

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     'use strict';
 
-    var forumFeedURL, forumURL, boxTemplate, $forum;
+    var forumFeedURL, forumURL, boxTemplate, formatShortTitle, $forum;
 
     forumFeedURL = 'https://forum.sabayon.org/feed.php';
     forumURL = 'https://forum.sabayon.org/';
@@ -15,14 +15,33 @@ $(document).ready(function() {
         '</div>'
     ].join('');
 
+    formatShortTitle = function(entry) {
+        var separator, shortTitle, topicPrefix;
+
+        separator = '•';
+        shortTitle = entry.title;
+        if (shortTitle.indexOf(separator) > -1) {
+            topicPrefix = new RegExp('.*' + separator);
+            shortTitle = shortTitle.replace(topicPrefix, '');
+        }
+
+        if (shortTitle.indexOf('Re:') > -1) {
+            shortTitle = shortTitle.replace('Re:', '');
+        }
+        return shortTitle.trim();
+    };
+
     $forum = $('.panel').find('a[href="' + forumURL + '"]');
     if ($forum.length > 0) {
         $(document.body).append('<script type="text/javascript" src="/js/jquery.rss.min.js"></script>');
         $forum.parents('.panel').after(boxTemplate);
         $('#latest-forum-posts').rss(forumFeedURL, {
             ssl: true,
-            entryTemplate: '<li><a href="{url}">{title}</a><br />{shortBodyPlain}</li>',
-            layoutTemplate: '<ul style="list-style:none;padding:0;">{entries}</ul>'
+            entryTemplate: '<li><a href="{url}">{shortTitle}</a><br />{shortBodyPlain}</li>',
+            layoutTemplate: '<ul style="list-style:none;padding:0;">{entries}</ul>',
+            tokens: {
+                shortTitle: formatShortTitle
+            }
         });
     }
 });
